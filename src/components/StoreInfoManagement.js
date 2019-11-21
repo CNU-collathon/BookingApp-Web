903105dcab463de6fc365fe3b9c908d9f391170c
SehyeongLee <shseoul14@gmail.com>
2019-11-21 14:59:52 +0900

    Add image Upload func
    DB로부터 이미지파일 주고 받고 하는 기능 추가

--- src/components/StoreInfoManagement.js
+++ src/components/StoreInfoManagement.js
@@ -1,60 +1,133 @@
 import React, {useState , useEffect} from 'react'
 import { Link, Route, BrowserRouter as Router } from "react-router-dom";
-import { Button , Form } from 'antd';
+import { Button , Form ,Upload, Icon } from 'antd';
+import FileBase64 from 'react-file-base64';
+import axios from 'axios';
 export default function StoreInfoManagement() {
-    const [SelfEmployedID, setSelfEmployedID] = useState('')
-    const [WorkPlaceID, setWorkPlaceID] = useState('')
-    const [Address,setAddress] = useState('')
-    const [WorkPlaceInfo,setWorkPlaceInfo] = useState('')
-    const onChangeSelfEmployedID = e => setSelfEmployedID(e.target.value);
-    const onChangeWorkPlaceID = e => setWorkPlaceID(e.target.value);
+
+    const backendURL = "http://localhost:3100/"
+    // const WorkPlaceID = 
+    
+    const [workPlaceInfo, setWorkPlaceInfo] = useState('')
+    const [address,setAddress] = useState('')
+    const [name,setName] = useState('')
+    const [category,setCategory] = useState('')
+    const [isEdit,setIsEdit] = useState('true')
+    const [image,setImage] = useState('')
     const onChangeAddress = e => setAddress(e.target.value);
     const onChangeWorkPlaceInfo = e => setWorkPlaceInfo(e.target.value);
+    const onChangeName = e => setName(e.target.value);
+    const onChangeCategory = e => setCategory(e.target.value);
     const goBack = () => {
       // {/* signIn process ... */}
     };
     const saveInfo = () => {
       // {/* saveInfo process ... */}
+      console.log({
+        SelfEmployedID:localStorage.getItem('localSelfId'),
+        Address:address,
+        WorkPlaceInfo: workPlaceInfo,
+        Name:name,
+        Category:category
+      })
+      axios.post(backendURL+"workplace",{
+        SelfEmployedID:localStorage.getItem('localSelfId'),
+        Address:address,
+        WorkPlaceInfo: workPlaceInfo,
+        Name:name,
+        Category:category,
+        Image:image
+      }).then( res => {
+        console.log('response', res)
+        alert("완료")
+      }).catch(err =>{
+        console.log('failed', err)
+      })  
     };
-    const validateSelfEmployedIDSelfEmployedID = () => {
-      // {/* SelfEmployedID valiDation/}
-    }
 
-    
-    const fromDB = {
-        "SelfEmployedID":"2",
-        "WorkPlaceID":"2",
-        "Address":"Daejeon",
-        "WorkPlaceInfo":"청춘우동까스",
+    const editInfo = () => {
+      // {/* saveInfo process ... */}
+      console.log({
+        SelfEmployedID:localStorage.getItem('localSelfId'),
+        Address:address,
+        WorkPlaceInfo: workPlaceInfo,
+        Name:name,
+        Category:category
+      })
+      axios.put(backendURL+"workplace/update/"+localStorage.getItem("WorkPlaceID"),{
+        SelfEmployedID:localStorage.getItem('localSelfId'),
+        Address:address,
+        WorkPlaceInfo: workPlaceInfo,
+        Name:name,
+        Category:category,
+        Image:image
+      }).then( res => {
+        console.log('response', res)
+        alert("완료")
+        setIsEdit("true")
+      }).catch(err =>{
+        console.log('failed', err)
+      })  
+    };
+
+    const getFiles = (file) => {
+        const uploadImage = {
+          File:file["base64"].split(",")[1],
+          FileName:file["name"],
+          Size:file['size'].split(" ")[0]
+        }
+        setImage(uploadImage)
+        console.log(uploadImage)
     }
 
     useEffect(() => {
-      console.log(1);
-      setSelfEmployedID(fromDB["SelfEmployedID"]);
-      setWorkPlaceID(fromDB["WorkPlaceID"]);
-      setAddress(fromDB["Address"]);
-      setWorkPlaceInfo(fromDB["WorkPlaceInfo"]);
-      console.log(1);
-    }, []);
+      axios.get(backendURL + "account/workplace/" + localStorage.getItem("localSelfId")).
+      then( response => {
+       console.log(response)
+       if(response.data.result===0){
+         setIsEdit("false")
+         console.log("등록모드")  
+       }
+       else{
+       console.log(response)
+       setAddress(response.data[0]["Address"]);
+       setWorkPlaceInfo(response.data[0]["WorkPlaceInfo"]);
+       setName(response.data[0]["Name"]);
+       setCategory(response.data[0]["Category"]);
+       setImage(response.data[0]["Image"])
+       localStorage.setItem("WorkPlaceID",response.data[0]["WorkPlaceID"])
+       console.log("수정모드" +localStorage.getItem("WorkPlaceID") )
+       }
+      }
+      )
+  }, [])
 
     return (
         <div>
             <h1>StoreInfoManagement</h1>
             <Form>
               <Form.Item>
-                SelfEmployedID <input type="text" value={SelfEmployedID} onChange={onChangeSelfEmployedID} required='true'></input>
+                SelfEmployedID : {localStorage.getItem('localSelfId')}
               </Form.Item>  
               <Form.Item>
-                WorkPlaceID <input type="WorkPlaceID" value={WorkPlaceID} onChange={onChangeWorkPlaceID} required='true'></input>
+              Address <input type="text" value={address} onChange={onChangeAddress}></input>
               </Form.Item>
               <Form.Item>
-              Address <input type="Address" value={Address} onChange={onChangeAddress}></input>
+                WorkPlaceInfo <input type="text" value={workPlaceInfo} onChange={onChangeWorkPlaceInfo}></input>
               </Form.Item>
               <Form.Item>
-                WorkPlaceInfo <input type="WorkPlaceInfo" value={WorkPlaceInfo} onChange={onChangeWorkPlaceInfo}></input>
+                Name <input type="text" value={name} onChange={onChangeName}></input>
               </Form.Item>
               <Form.Item>
-                 <Button onClick={saveInfo} htmlType="submit" >Save</Button> <Link to='/SignIn'><Button onClick={goBack}>뒤로가기</Button></Link>
+                Category <input type="text" value={category} onChange={onChangeCategory}></input>
+              </Form.Item>
+              Store Image <br/>
+              <FileBase64
+                  multiple={ false } onDone={ getFiles.bind(this) } ></FileBase64>
+              <img height="300px" width="300px" src={`data:image/jpeg;base64,${image.File}`} />
+              <Form.Item>
+                 <Button onClick={ isEdit=="true" ? editInfo : saveInfo }>
+                   Save</Button><Link to='/SignIn'><Button onClick={goBack}>뒤로가기</Button></Link>
               </Form.Item>
             </Form>
         </div>
