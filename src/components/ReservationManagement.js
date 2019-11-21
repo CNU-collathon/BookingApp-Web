ee6e1fd36a92d9f02151e7143277a79b200abd45
SehyeongLee <shseoul14@gmail.com>
2019-11-21 15:00:56 +0900

    Add exeception handle
    예약정보가 없을 때 예외처리 추가

--- src/components/ReservationManagement.js
+++ src/components/ReservationManagement.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import Reservation from './Reservation';
 export default function ReservationManagement() {
     const backendURL = "http://localhost:3100/"
-    const WorkPlaceID = "4d4525fb"
+    const WorkPlaceID = localStorage.getItem("WorkPlaceID")
     // const [ID, setID] = useState('')
     // const [WorkPlaceID, setWorkPlaceID] = useState('')
     // const [MenuNames, setMenuNames] = useState('')
@@ -68,21 +68,27 @@ export default function ReservationManagement() {
         axios.get(backendURL + "workplace/reservation/" + WorkPlaceID).
         then( response => {
             console.log(response)
-            const Reservations = response.data.map(
-                ReservationManagement => {
-                    return(
-                        <Reservation ID={ReservationManagement["ID"]} 
-                                     Menu={ReservationManagement["Menu"]}
-                                     Price={ReservationManagement["Price"]}
-                                     Personnel={ReservationManagement["Personnel"]}
-                                     ReservedTime={ReservationManagement["ReservedDateTime"]}
-                                     EndDateTime={ReservationManagement["EndDateTime"]}
-                                     Detail={ReservationManagement["Detail"]}>
-                        </Reservation>             
-                    )
-                }
-            )
-            setData(Reservations)
+            if(response.data.result===0){
+                const Reservations = <h1>예약정보가 존재하지 않습니다.</h1>
+                setData(Reservations)
+            }
+            else{
+                const Reservations = response.data.map(
+                    ReservationManagement => {
+                        return(
+                            <Reservation ID={ReservationManagement["ID"]} 
+                                         Menu={ReservationManagement["Menu"]}
+                                         Price={ReservationManagement["Price"]}
+                                         Personnel={ReservationManagement["Personnel"]}
+                                         ReservedTime={ReservationManagement["ReservedDateTime"]}
+                                         EndDateTime={ReservationManagement["EndDateTime"]}
+                                         Detail={ReservationManagement["Detail"]}>
+                            </Reservation>             
+                        )
+                    }
+                )
+                setData(Reservations)
+            }
             }
         )
     }, [])
